88e9d0dfdd77aca169135b3749a214cfcbfa3b17
John Ellmore <495475+johnellmore@users.noreply.github.com>
2023-04-08 13:58:28 -0500

    most of rendered

--- lib.mjs
+++ lib.mjs
@@ -1,3 +1,7 @@
+const STROKE_COLOR = 'black'; // CSS color
+const STROKE_WIDTH = 0.01; // percentage of page width
+const SEPARATORS = ['/', '|', '\\'];
+
 export function generateRandomBook(numPages) {
   const rowsPerPage = [2, 3];
   const pages = range(numPages).map(p => {
@@ -18,10 +22,9 @@ function generateRandomPageRow() {
   ];
   const thisRowWidth = pickRandom(rowWidths);
 
-  const separators = ['/', '|', '\\'];
   let row = '' + thisRowWidth.shift();
   for (const width of thisRowWidth) {
-    const sep = pickRandom(separators);
+    const sep = pickRandom(SEPARATORS);
     row += sep + width;
   }
   return row;
@@ -35,6 +38,90 @@ function pickRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-export class ComicPage {
+class Page {
+  constructor(rows, innerMargin, outerMargin) {
+    this.rows = rows;
+    this.innerMargin = innerMargin;
+    this.outerMargin = outerMargin;
+  }
+}
 
+class PageRow {
+  constructor(panels) {
+    this.panels = panels;
+  }
+}
+
+class Panel {
+  constructor(width, leftBorderType, rightBorderType) {
+    if (typeof width !== 'number') {
+      throw new Error('width is not a number');
+    }
+    this.width = width;
+    this.leftBorderType = leftBorderType;
+    this.rightBorderType = rightBorderType;
+  }
+}
+
+export function parseTemplate(tmpl) {
+  const PAGE_INNER_MARGIN = 0.05; // as percentage of page width
+  const PAGE_OUTER_MARGIN = 0.1; // as percentage of page width
+  return tmpl.split("\n\n").map(
+    pageTmpl => new Page(pageTmpl.split("\n").map(
+      rowTmpl => {
+        const chars = (rowTmpl + '|').split('');
+        let lastBorderType = '|';
+        let lastPanelWidth = null;
+        const panels = [];
+        for (const ch of chars) {
+          if (!isNaN(+ch)) { // if is numeric
+            lastPanelWidth = +ch;
+          } else if (SEPARATORS.includes(ch)) {
+            panels.push(new Panel(lastPanelWidth, lastBorderType, ch));
+            lastPanelWidth = null;
+            lastBorderType = ch;
+          } else {
+            console.log(rowTmpl);
+            console.log(ch);
+            throw new Error('Parse error');
+          }
+        }
+        return new PageRow(panels);
+      }),
+      PAGE_INNER_MARGIN,
+      PAGE_OUTER_MARGIN
+    ));
+}
+
+export function pageToFrames(page, pageSizeWH) {
+  const [wPx, hPx] = pageSizeWH;
+  return [
+    [
+      [wPx * .1, hPx * .1],
+      [wPx * .1, hPx * .9],
+      [wPx * .9, hPx * .9],
+      [wPx * .9, hPx * .1],
+    ]
+  ];
+}
+
+export function renderPageFrames(polygons, pageSizeWH) {
+  const [wPx, hPx] = pageSizeWH;
+
+  const cv = document.createElement('canvas');
+  cv.width = wPx;
+  cv.height = hPx;
+  const ctx = cv.getContext('2d');
+  ctx.strokeStyle = "#000000";
+  ctx.lineWidth = STROKE_WIDTH * wPx;
+  polygons.forEach(points => {
+    ctx.beginPath();
+    const start = points.shift();
+    ctx.moveTo(...start);
+    points.forEach(p => ctx.lineTo(...p));
+    ctx.closePath();
+    ctx.stroke();
+  });
+
+  return cv;
 }
